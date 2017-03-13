6f42bcf82b0f13525bb1045be9a6520c98f7630f
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-03-13 12:24:19 +0200

    rollup: add separate example config

--- rollup.example.js
+++ rollup.example.js
@@ -3,13 +3,13 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
   entry: 'index.es',
-  dest: 'dist/otto2d.js',
-  format: 'iife',
-  indent: true,
-  sourceMap: true,
-  moduleName: 'Otto2d',
   plugins: [
     babel(),
     nodeResolve(),
   ],
+  format: 'iife',
+  indent: true,
+  interop: false,
+  moduleName: 'Otto2d',
+  dest: 'example/otto2d.js',
 };
