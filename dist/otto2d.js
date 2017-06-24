fc7b8776fdb9fd2d577e6c43eea372e8bb95118c
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-06-24 23:30:45 +0200

    module: drop out of default params, rebuild, add dist

--- dist/otto2d.js
+++ dist/otto2d.js
@@ -4,13 +4,13 @@ var Otto2d = (function (Otto) {
 // # Otto 2d
 // Helps create CA grids
 
-var Otto2d = function Otto2d() {
-  var data = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : { size: 1 };
+var Otto2d = function Otto2d(data) {
+  var size = data && data.size || 1;
+  var area = { size: size * size };
 
-  var area = { size: data.size * data.size };
   var otto = Object.assign({
     rule: 614,
-    ends: [-1, 1, -data.size, data.size],
+    ends: [-1, 1, -size, size],
     stat: function stat(hood, code, flag) {
       return code[flag + hood.reduce(function (a, b) {
         return a + b;
--- index.es
+++ index.es
@@ -3,11 +3,13 @@
 
 import Otto from '@thewhodidthis/otto';
 
-const Otto2d = (data = { size: 1 }) => {
-  const area = { size: data.size * data.size };
+const Otto2d = (data) => {
+  const size = (data && data.size) || 1;
+  const area = { size: size * size };
+
   const otto = Object.assign({
     rule: 614,
-    ends: [-1, 1, -data.size, data.size],
+    ends: [-1, 1, -size, size],
     stat: (hood, code, flag) => code[flag + (hood.reduce((a, b) => a + b) * 2)],
   }, data, area);
 
