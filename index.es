34f5709a532e3cf3415ff0d0077de94e8f8e9254
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-07-13 11:10:29 +0200

    module: relint

--- index.es
+++ index.es
@@ -1,20 +1,19 @@
 // # Otto 2d
 // Helps create CA grids
 
-import Otto from '@thewhodidthis/otto';
+import Otto from '@thewhodidthis/otto'
 
 const Otto2d = (data) => {
-  const size = (data && data.size) || 1;
-  const area = { size: size * size };
+  const size = (data && data.size) || 1
+  const area = { size: size * size }
 
   const t0to = Object.assign({
     rule: 614,
     ends: [-1, 1, -size, size],
-    stat: (hood, code, flag) => code[flag + (hood.reduce((a, b) => a + b) * 2)],
-  }, data, area);
+    stat: (hood, code, flag) => code[flag + (hood.reduce((a, b) => a + b) * 2)]
+  }, data, area)
 
-  return Otto(t0to);
-};
+  return Otto(t0to)
+}
 
-export default Otto2d;
-
+export default Otto2d
--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 'use strict';
 
-var Otto = require('@thewhodidthis/otto');
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var Otto = _interopDefault(require('@thewhodidthis/otto'));
 
 // # Otto 2d
 // Helps create CA grids
 
-var Otto2d = function Otto2d(data) {
-  var size = data && data.size || 1;
+var Otto2d = function (data) {
+  var size = (data && data.size) || 1;
   var area = { size: size * size };
 
   var t0to = Object.assign({
     rule: 614,
     ends: [-1, 1, -size, size],
-    stat: function stat(hood, code, flag) {
-      return code[flag + hood.reduce(function (a, b) {
-        return a + b;
-      }) * 2];
-    }
+    stat: function (hood, code, flag) { return code[flag + (hood.reduce(function (a, b) { return a + b; }) * 2)]; }
   }, data, area);
 
-  return Otto(t0to);
+  return Otto(t0to)
 };
 
 module.exports = Otto2d;
+
