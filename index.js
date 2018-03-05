a138a931147365e5f973ea8305bdd5c27c6cc939
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2018-03-05 06:22:18 +0100

    module: buble not the cjs version

--- index.js
+++ index.js
@@ -3,20 +3,18 @@
 var otto = require('@thewhodidthis/otto');
 
 // # Otto 2d
-// Helps create CA grids
 
-var otto2d = function (from) {
-  var size = (from && from.size) || 1;
-  var grid = { size: size * size };
+const otto2d = (from) => {
+  const size = (from && from.size) || 1;
+  const grid = { size: size * size };
 
-  var data = Object.assign({
+  const data = Object.assign({
     rule: 614,
     ends: [-1, 1, -size, size],
-    stat: function (hood, code, flag) { return code[flag + (hood.reduce(function (a, b) { return a + b; }) * 2)]; }
+    stat: (hood, code, flag) => code[flag + (hood.reduce((a, b) => a + b) * 2)]
   }, from, grid);
 
   return otto(data)
 };
 
 module.exports = otto2d;
-
